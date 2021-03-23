f2021299e138b2b449991b02f3ec89c69ce2f67f
Bugato <thomas.retraint@gmail.com>
2021-03-23 10:56:18 +0000

    Update check_gpu.py

--- check_gpu.py
+++ check_gpu.py
@@ -1,7 +1,2 @@
-#from tensorflow.python.client import device_lib
-#print(device_lib.list_local_devices())
-
-import os
-print(os.path.splitext('./pull_up_data/keypoints/video1_1.csv')[0])
-test = os.path.splitext('./pull_up_data/keypoints/video1_1.csv')[0]
-print(os.path.basename(os.path.normpath(test)))
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
